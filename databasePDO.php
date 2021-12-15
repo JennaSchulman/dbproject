74b2bcc43287081d6a99cfc53dc60f89afa29cce
JennaSchulman <jenna.e.schulman@gmail.com>
2021-12-15 12:35:05 -0500

    fixed deleting productamountsold

--- databasePDO.php
+++ databasePDO.php
@@ -1,7 +1,7 @@
 <?php
 $dsn='mysql:host=localhost;dbname=amusementpark';
 try {
-  $db = new PDO($dsn, "root", "");
+  $db = new PDO($dsn, "root", "root");
 } catch (PDOException $e) {
   $error=$e->getMessage();
   echo '<p> Unable to connect to database: '.$error;
--- delete_product.php
+++ delete_product.php
@@ -2,12 +2,21 @@
 require('databasePDO.php');
 
 $id = $_REQUEST['id'];
+
+$query = "DELETE FROM productamountsold WHERE productID = :id";
+
+$statement = $db->prepare($query);
+$statement->bindValue(':id', $id);
+$statement->execute();
+$statement->closeCursor();
+
 $query = "DELETE FROM products WHERE productID = :id";
 
 $statement = $db->prepare($query);
 $statement->bindValue(':id', $id);
 $statement->execute();
 $statement->closeCursor();
+
 
 header("Location: products.php");
 exit();
